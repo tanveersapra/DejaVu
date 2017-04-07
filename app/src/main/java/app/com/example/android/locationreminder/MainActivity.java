8562f7d2b332f8339fa0e4c268d52fe0181575d4
Navneet Jain <Navneet Jain>
2017-04-07 23:41:12 +0530

    hello

--- app/src/main/java/app/com/example/android/locationreminder/MainActivity.java
+++ app/src/main/java/app/com/example/android/locationreminder/MainActivity.java
@@ -1,5 +1,6 @@
 package app.com.example.android.locationreminder;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -7,62 +8,72 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    private RecyclerView recyclerView;
-    private RecyclerView.Adapter adapter;
-    private RecyclerView.LayoutManager layoutManager;
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        String note_names[]={"First Note","Sumeet ka pyar","Tanveer ka lund","Navneet is cool","Art Appreciation","Depressing Sights",
-                "Depressing Nights","Things that scare me","Rakshason ka Jhund","bkl = ?"};
-        String note_summary[]={"bola tha this is my first note chutiya","jo kisi ko na mila","jo kisiko na dikha, size matters","badi obvious si baat hain why repeat",
-        "Picasso, Donatello, Raphael, Michaelangelo, Leonardo, Pogba","Andhere mein girlfriend na dikhi","But alcohol shall cure me tonight",
-        "Things between your legs, things that come out from tere","Tanny rules the roost","abbey behen ke laude, badda khaas launda"};
-        recyclerView=(RecyclerView)findViewById(R.id.recycler_view);
-        adapter=new RecyclerAdapter(note_names,note_summary);
-        layoutManager=new LinearLayoutManager(this);
-        recyclerView.setLayoutManager(layoutManager);
-        recyclerView.setHasFixedSize(true);
-        recyclerView.setAdapter(adapter);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+	private RecyclerView recyclerView;
+	private RecyclerView.Adapter adapter;
+	private RecyclerView.LayoutManager layoutManager;
+	String note_names[] = {"First Note", "Sumeet ka pyar", "Tanveer ka lund", "Navneet is cool", "Art Appreciation", "Depressing Sights",
+			"Depressing Nights", "Things that scare me", "Rakshason ka Jhund", "bkl = ?"};
+	String note_summary[] = {"bola tha this is my first note chutiya", "jo kisi ko na mila", "jo kisiko na dikha, size matters", "badi obvious si baat hain why repeat",
+			"Picasso, Donatello, Raphael, Michaelangelo, Leonardo, Pogba", "Andhere mein girlfriend na dikhi", "But alcohol shall cure me tonight",
+			"Things between your legs, things that come out from tere", "Tanny rules the roost", "abbey behen ke laude, badda khaas launda"};
+	int note_type[]={1,1,2,2,1,1,2,2,2,1};
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+		recyclerView = (RecyclerView) findViewById(R.id.recycler_view);
+		adapter = new RecyclerAdapter(note_names, note_summary,note_type, getApplicationContext());
+		layoutManager = new LinearLayoutManager(this);
+		recyclerView.setLayoutManager(layoutManager);
+		recyclerView.setHasFixedSize(true);
+		recyclerView.setAdapter(adapter);
+		Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+		setSupportActionBar(toolbar);
+
+		FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+		fab.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View view) {
+				Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+						.setAction("Action", null).show();
+			}
+		});
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.menu_main, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Handle action bar item clicks here. The action bar will
+		// automatically handle clicks on the Home/Up button, so long
+		// as you specify a parent activity in AndroidManifest.xml.
+		int id = item.getItemId();
+
+		//noinspection SimplifiableIfStatement
+		if (id == R.id.action_settings) {
+			return true;
+		}
+
+		return super.onOptionsItemSelected(item);
+	}
+    public void new_note(View view)
+    {
+	    //Toast.makeText(this,"You clicked on the plus sign",Toast.LENGTH_SHORT).show();
+        Intent i=new Intent(this,new_note_adder.class);
+        startActivity(i);
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
 }
