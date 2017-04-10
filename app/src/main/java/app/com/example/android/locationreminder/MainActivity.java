a2984efd9ab6cb3d175db049d3870e8064b0609b
Sumeet Bahadkar <Sumeet Bahadkar>
2017-04-11 03:23:51 +0530

    Location Listener

--- app/src/main/java/app/com/example/android/locationreminder/MainActivity.java
+++ app/src/main/java/app/com/example/android/locationreminder/MainActivity.java
@@ -71,6 +71,7 @@ public class MainActivity extends AppCompatActivity {
         if(ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED){
             ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 0);
         }
+        startService(new Intent(this, ListenerService.class));
     }
 
     @Override
